da92a41bca5f2bfb306e827094661ab0dfd01588
ade.baeyee <ade.baeyee@97007ed5-9aaf-9a57-90b1-4861f2c528ca>
2010-12-20 11:34:22 +0000

    initial commit reader.java to handle content synopsis and calculate weight

---
+++ src/core/engines/Reader.java
@@ -0,0 +1,15 @@
+package core.engines;
+
+public class Reader {
+		
+	String content[];
+	
+	Reader(String [] content){
+		this.content = content;
+		System.out.println("reader contructor called");
+	}
+	
+	public void parseParagraphToKalimat(int posparagraph, int poskalimat){
+		
+	}
+}
