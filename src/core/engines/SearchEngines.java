3ff1b596d9c01843d94c65ff74fbe66a9e65f072
ade.baeyee <ade.baeyee@97007ed5-9aaf-9a57-90b1-4861f2c528ca>
2010-12-20 11:22:40 +0000

    update searchengines servlet to get all <p> synopsis content

--- src/core/engines/SearchEngines.java
+++ src/core/engines/SearchEngines.java
@@ -3,10 +3,8 @@ package core.engines;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Proxy;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -48,9 +46,14 @@ public class SearchEngines extends HttpServlet {
 
 	    BufferedReader in = new BufferedReader(new InputStreamReader(theurl.openStream()));
 	    String line;
+	    int id = 0;
+	    String content[] = new String [1000];
 	    while ((line = in.readLine())!=null){
 	    	line = line.trim();
-	    	if (Pattern.matches("<p>", line)){
+	    	//System.out.println(line);
+	    	if (line.matches("<p>[A-Z](.*)")){
+	    		content[id] = line;
+	    		id++;
 	    		System.out.println(line);
 	    	}
 	    }
