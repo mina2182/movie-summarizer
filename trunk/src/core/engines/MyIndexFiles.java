c36974cba23e4f22ac9a79256f21a07afe2e7ac9
ade.baeyee <ade.baeyee@97007ed5-9aaf-9a57-90b1-4861f2c528ca>
2010-12-23 12:05:13 +0000

    update index implementation update calculate top term
    map alpahbetical order

--- trunk/src/core/engines/MyIndexFiles.java
+++ trunk/src/core/engines/MyIndexFiles.java
@@ -2,14 +2,27 @@ package core.engines;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Index;
+import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.index.TermEnum;
 
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.LockObtainFailedException;
+import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
 
@@ -23,6 +36,7 @@ public class MyIndexFiles {
 	
 	
 	public MyIndexFiles(String [] content){
+		ramdir = new RAMDirectory();
 		this.content = content;
 	}
 	
@@ -63,12 +77,25 @@ public class MyIndexFiles {
 		print();
 	}
 	
+	public float calcBoost(int posparagraph, int possentence){
+		float score = 0.5F;
+		return score;
+	}
+	
 	@SuppressWarnings("deprecation")
 	public void index(){
 		try {
 			IndexWriter writer = new IndexWriter(ramdir, new StandardAnalyzer(Version.LUCENE_CURRENT), true, IndexWriter.MaxFieldLength.UNLIMITED);
-			
-			writer.optimize();
+			for (int ii = 0; ii< list.size(); ii++){
+				ArrayList<String> temp = list.get(ii);
+				for (int jj = 0; jj < temp.size(); jj++){
+					temp.get(jj);
+					Document doc = new Document();
+					doc.add(new Field("text",temp.get(jj), Store.YES, Index.ANALYZED));
+					doc.setBoost(calcBoost(ii,jj));
+					writer.addDocument(doc);
+				}
+			}
 			writer.commit();
 			writer.close();
 			 
@@ -82,5 +109,40 @@ public class MyIndexFiles {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public Query computeTopTermQuery(){
+		try {
+			final Map<String,Integer> frequencyMap = new HashMap<String,Integer>();
+			ArrayList<String> termlist = new ArrayList<String>();
+			IndexReader reader = IndexReader.open(ramdir);
+			TermEnum terms = reader.terms();
+			while (terms.next()){
+				Term term = terms.term();
+				String termtext = term.text();
+				int freq = reader.docFreq(term);
+				frequencyMap.put(termtext, freq);
+				termlist.add(termtext);
+			}
+			reader.close();
+			
+			StringBuilder termbuf = new StringBuilder();
+			BooleanQuery q = new BooleanQuery();
+			for (String topterm : termlist){
+				termbuf.append(topterm).append("(").append(frequencyMap.get(topterm)).append(")");
+				q.add(new TermQuery(new Term("text",topterm)),Occur.SHOULD);
+			}
+			System.out.println(">>> top term : " + termbuf.toString());
+			System.out.println(">>> query : " + q.toString());
+			return q;
+			
+		} catch (CorruptIndexException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
